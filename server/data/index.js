42cde665c03c19d3361a1c8891485ee80113064c
1t1e1 <1t1ebir@gmail.com>
2020-02-17 23:55:39 +0300

    adding some example data

--- server/data/index.js
+++ server/data/index.js
@@ -1,20 +1,72 @@
 export default [
     {
+        id: 0,
+        author: "J.K. Rowling",
+        book: "Harry Potter and the Goblet of Fire",
+        quote:
+            "If you want to know what a man's like, take a good look at how he treats his inferiors, not his equals.",
+    },
+    {
         id: 1,
-        author: "yazar",
-        book: "kitap",
-        quote: "alinti",
+        author: "Douglas Adams",
+        book: undefined,
+        quote:
+            "It's not the fall that kills you; it's the sudden stop at the end.",
     },
     {
         id: 2,
-        author: "yazar",
-        book: "kitap",
-        quote: "alinti",
+        author: "George Orwell",
+        book: undefined,
+        quote:
+            "Political language is designed to make lies sound truthful and murder respectable, and to give an appearance of solidity to pure wind.",
     },
     {
         id: 3,
-        author: "yazar",
-        book: "kitap",
-        quote: "alinti",
+        author: "J.R.R. Tolkien",
+        book: "The Fellowship of the Ring",
+        quote:
+            '"I wish it need not have happened in my time," said Frodo. "So do I," said Gandalf, "and so do all who live to see such times. But that is not for them to decide. All we have to decide is what to do with the time that is given us."',
+    },
+    {
+        id: 4,
+        author: "George R.R. Martin",
+        book: "A Dance with Dragons",
+        quote:
+            "A reader lives a thousand lives before he dies, said Jojen. The man who never reads lives only one.",
+    },
+    {
+        id: 5,
+        author: "Ernest Hemingway",
+        book: "A Farewell to Arms",
+        quote:
+            "The world breaks everyone and afterward many are strong at the broken places. But those that will not break it kills. It kills the very good and the very gentle and the very brave impartially. If you are none of these you can be sure it will kill you too but there will be no special hurry.",
+    },
+    {
+        id: 6,
+        author: "Ernest Hemingway",
+        book: "A Farewell to Arms",
+        quote:
+            "The world breaks everyone and afterward many are strong at the broken places. But those that will not break it kills. It kills the very good and the very gentle and the very brave impartially. If you are none of these you can be sure it will kill you too but there will be no special hurry.",
+    },
+    {
+        id: 7,
+        author: "Ernest Hemingway",
+        book: "A Farewell to Arms",
+        quote:
+            "The world breaks everyone and afterward many are strong at the broken places. But those that will not break it kills. It kills the very good and the very gentle and the very brave impartially. If you are none of these you can be sure it will kill you too but there will be no special hurry.",
+    },
+    {
+        id: 8,
+        author: "Ernest Hemingway",
+        book: "A Farewell to Arms",
+        quote:
+            "The world breaks everyone and afterward many are strong at the broken places. But those that will not break it kills. It kills the very good and the very gentle and the very brave impartially. If you are none of these you can be sure it will kill you too but there will be no special hurry.",
+    },
+    {
+        id: 9,
+        author: "Ernest Hemingway",
+        book: "A Farewell to Arms",
+        quote:
+            "The world breaks everyone and afterward many are strong at the broken places. But those that will not break it kills. It kills the very good and the very gentle and the very brave impartially. If you are none of these you can be sure it will kill you too but there will be no special hurry.",
     },
 ];
