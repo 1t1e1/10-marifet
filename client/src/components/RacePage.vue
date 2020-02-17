fdaed7d4e2f1c6219bd4ae96628bffa04ff206d4
1t1e1 <1t1ebir@gmail.com>
2020-02-17 23:58:23 +0300

    vue little start

---
+++ client/src/components/RacePage.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="onparmak">
+        <p>{{ data }}</p>
+        <p>{{ info }}</p>
+        <button @click="clicked">++</button>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+    name: "HelloWorld",
+    data() {
+        return {
+            data: "ne e",
+            info: "",
+        };
+    },
+    mounted() {
+        axios
+            .get("http://localhost:3000/3")
+            .then(response => (this.info = response.data.response));
+        console.log("hell now");
+    },
+    methods: {
+        clicked: function() {
+            let maxId = 5;
+            let randomId = Math.floor(Math.random() * maxId);
+            console.log("clickedd ,", randomId);
+        },
+    },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,27 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from "vue";
+import Router from "vue-router";
+import HelloWorld from "@/components/HelloWorld";
+import Deneme from "@/components/Deneme";
+import RacePage from "@/components/RacePage";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
-})
+    routes: [
+        {
+            path: "/deneme",
+            name: "Deneme",
+            component: Deneme,
+        },
+        {
+            path: "/hello",
+            name: "HelloWorld",
+            component: HelloWorld,
+        },
+        {
+            path: "/race",
+            name: "race",
+            component: RacePage,
+        },
+    ],
+});
