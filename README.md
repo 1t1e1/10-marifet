b89307a743fbd7e5ad36b8eb0a0832ca7bc92616
1t1e1 <1t1ebir@gmail.com>
2020-02-17 16:48:31 +0300

    init server and readme.md

---
+++ README.md
@@ -0,0 +1,10 @@
+# 10 parmakta 10 mariget
+
+Basit bir 10 parmak klavye projesidir.
+Kitaplardan alintilari yazarak hizlinizi olcmeyi saglar.
+
+## Backend
+
+`node.js` ile `sqlite` ve `mongodb` kullanarak bana alintilari gonderen bir server yazacagim.
+
+## Frontend
---
+++ server/index.js
@@ -0,0 +1,14 @@
+var express = require("express");
+import cors from "cors";
+// var cors = require("cors");
+var app = express();
+
+app.use(cors());
+
+app.get("/:id", function(req, res, next) {
+    res.json({ msg: "This is CORS-enabled for all origins!" });
+});
+
+app.listen(3000, function() {
+    console.log("CORS-enabled web server listening on port 3000");
+});
